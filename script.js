740448db95b39a8896aed3aec42fdbaf85c3bc9a
Midnight <pedrorodacinski26@gmail.com>
2025-06-13 10:07:38 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
 document.addEventListener("DOMContentLoaded", () => {
     const btn = document.getElementById("google-btn");
+    const btn2 = document.getElementById("apple-btn");
     
     const carregarHome = () => {
       window.location.href = "telas/home.html"; // Redireciona para home.html
     };
 
     btn.addEventListener("click", carregarHome);
+    btn2.addEventListener("click", carregarHome);
   });
